9b23f7f195d2acbc77e2ec6ba925cb8459ddff43
Soumt <soumt@dimigo.hs.kr>
2023-03-06 10:59:52 +0900

    fix: audio -> p

--- speech_recognition/__init__.py
+++ speech_recognition/__init__.py
@@ -149,7 +149,7 @@ class Microphone(AudioSource):
             result = []
             for i in range(0, info.get('deviceCount')):
                 if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
-                    device_info = audio.get_device_info_by_index(i)
+                    device_info = p.get_device_info_by_index(i)
                     result.append(device_info.get("name"))
         finally:
             p.terminate()
