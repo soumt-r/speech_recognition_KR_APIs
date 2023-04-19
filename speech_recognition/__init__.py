8458d6bb1fbdf3c77ccdc732c4e3345779cb5383
Soumt <soumt@dimigo.hs.kr>
2023-04-19 15:30:08 +0900

    fix: fixed version issue

--- speech_recognition/__init__.py
+++ speech_recognition/__init__.py
@@ -27,7 +27,7 @@ except (ModuleNotFoundError, ImportError):
 
 __author__ = "Anthony Zhang (Uberi)"
 
-__version__ = "3.10.0.dev1"
+__version__ = "3.10.0"
 __license__ = "BSD"
 
 from urllib.parse import urlencode
